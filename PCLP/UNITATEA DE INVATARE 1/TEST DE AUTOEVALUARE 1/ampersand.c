253612e6fd3a83145fb443b8d3cc1416acae3de9
Dacian Florin Dedu <119755646+DacianFD84@users.noreply.github.com>
2024-11-25 14:07:11 +0200

    &

---
+++ PCLP/UNITATEA DE INVATARE 1/TEST DE AUTOEVALUARE 1/ampersand.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+    printf("Adresa lui x este: %p\n", &x); // `%p` pentru a afișa adresa de memorie
+    printf("Variabila are valoarea: %d", x);
+    return 0;
+}
